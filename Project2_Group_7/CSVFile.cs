5296830444471e0237dd974a51bded2733d1d200
JeffN129334 <140469753+JeffN129334@users.noreply.github.com>
2024-03-25 00:49:24 -0400

    Add files via upload

---
+++ Project2_Group_7/CSVFile.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project2_Group_7
+{
+    public static class CSVFile
+    {
+        public static List<string> ParseCSV(string fileName)
+        {
+            //String list to store expressions
+            List<string> expressionList = new List<string>();
+
+            //Read all the text from the file
+            string csvContent = File.ReadAllText(fileName);
+
+            //Split the text into rows
+            string[] rows = csvContent.Split('\n');
+            string[] fields;
+
+            //For each row (minus the first one which contains the headers)
+            for (int i = 1; i < rows.Length - 1; i++)
+            {
+                //Split the rows into fields
+                fields = rows[i].Split(',');
+
+                //Create a new CityInfo object
+                expressionList.Add(fields[1]);
+            }
+
+            //If the list is empty throw an exception
+            if (expressionList.Count <= 0)
+            {
+                throw new Exception("No data found");
+            }
+            return expressionList;
+        }
+    }
+}
---
+++ Project2_Group_7/Program.cs
@@ -0,0 +1,48 @@
+﻿using System.Diagnostics;
+using System.Runtime;
+
+namespace Project2_Group_7
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputFile = "../../../Data/Project 2_INFO_5101.csv";
+            string outputFile = "../../../Data/Output.xml";
+
+            //Parse CSV to string list and output it
+            List<string> list = CSVFile.ParseCSV(inputFile);
+            foreach (string item in list)
+            {
+                Console.WriteLine(item);
+            }
+
+
+            //Write stuff to an XML file
+            using (StreamWriter writer = new StreamWriter(outputFile))
+            {
+                writer.WriteStartDocument();
+                writer.WriteStartRootElement();
+                writer.WriteStartElement();
+                writer.WriteAttribute();        //does nothing yet
+                writer.WriteEndElement();
+                writer.WriteEndRootElement();
+            }
+
+            //Open the XML file
+            try
+            {
+                ProcessStartInfo info = new ProcessStartInfo
+                {
+                    UseShellExecute = true,
+                    FileName = Path.GetFullPath(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, outputFile))
+                };
+                Process.Start(info);
+            }
+            catch (Exception ex)
+            {
+                    Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
