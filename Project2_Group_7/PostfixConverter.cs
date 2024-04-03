f80b40f9959cc2eb15575d336552a80e31ad4ef8
Gui Miranda <gccardosomiranda@outlook.com>
2024-04-03 12:16:19 -0400

    Implements Prefix and Postfix converters

---
+++ Project2_Group_7/PostfixConverter.cs
@@ -0,0 +1,63 @@
+﻿using System.Text;
+
+namespace Project2_Group_7
+{
+    public class PostfixConverter
+    {
+        public string ConvertToPostfix(string infixExpression)
+        {
+            Stack<char> operators = new Stack<char>();
+            StringBuilder postfix = new StringBuilder();
+
+            foreach (char c in infixExpression)
+            {
+                if (c == '(')
+                {
+                    operators.Push(c);
+                }
+                else if (c == ')')
+                {
+                    while (operators.Count > 0 && operators.Peek() != '(')
+                        postfix.Append(operators.Pop());
+                    operators.Pop(); // Discard '('
+                }
+                else if (IsOperator(c))
+                {
+                    while (operators.Count > 0 && Priority(c) <= Priority(operators.Peek()))
+                        postfix.Append(operators.Pop());
+                    operators.Push(c);
+                }
+                else if (char.IsLetterOrDigit(c))
+                {
+                    postfix.Append(c);
+                }
+            }
+
+            // Append remaining operators
+            while (operators.Count > 0)
+                postfix.Append(operators.Pop());
+
+            return postfix.ToString();
+        }
+
+        private int Priority(char op)
+        {
+            switch (op)
+            {
+                case '+':
+                case '-':
+                    return 1;
+                case '*':
+                case '/':
+                    return 2;
+                default:
+                    return 0; // Parentheses
+            }
+        }
+
+        private bool IsOperator(char c)
+        {
+            return c == '+' || c == '-' || c == '*' || c == '/';
+        }
+    }
+}
---
+++ Project2_Group_7/PrefixConverter.cs
@@ -0,0 +1,67 @@
+﻿using System.Text;
+
+namespace Project2_Group_7
+{
+    public class PrefixConverter
+    {
+        public string ConvertToPrefix(string infixExpression)
+        {
+            Stack<char> operators = new Stack<char>();
+            StringBuilder prefix = new StringBuilder();
+
+            // Reverse the infix expression to facilitate conversion
+            char[] infixArray = infixExpression.ToCharArray();
+            Array.Reverse(infixArray);
+            infixExpression = new string(infixArray);
+
+            foreach (char c in infixExpression)
+            {
+                if (c == '(')
+                {
+                    while (operators.Count > 0 && operators.Peek() != ')')
+                        prefix.Append(operators.Pop());
+                    operators.Pop(); // Discard ')'
+                }
+                else if (IsOperator(c))
+                {
+                    while (operators.Count > 0 && Priority(c) < Priority(operators.Peek()))
+                        prefix.Append(operators.Pop());
+                    operators.Push(c);
+                }
+                else if (char.IsLetterOrDigit(c))
+                {
+                    prefix.Append(c);
+                }
+            }
+
+            // Append remaining operators
+            while (operators.Count > 0)
+                prefix.Append(operators.Pop());
+
+            // Reverse the result to get the prefix expression
+            char[] prefixArray = prefix.ToString().ToCharArray();
+            Array.Reverse(prefixArray);
+            return new string(prefixArray);
+        }
+
+        private int Priority(char op)
+        {
+            switch (op)
+            {
+                case '+':
+                case '-':
+                    return 1;
+                case '*':
+                case '/':
+                    return 2;
+                default:
+                    return 0; // Parentheses
+            }
+        }
+
+        private bool IsOperator(char c)
+        {
+            return c == '+' || c == '-' || c == '*' || c == '/';
+        }
+    }
+}
