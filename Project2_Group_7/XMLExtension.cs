0d04d118b251a05147ee6713e73cc53a9209432d
Gui Miranda <gccardosomiranda@outlook.com>
2024-04-03 12:07:25 -0400

    Implements WriteAttribute on XMLExtension

--- Project2_Group_7/XMLExtension.cs
+++ Project2_Group_7/XMLExtension.cs
@@ -1,38 +1,35 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Project2_Group_7
+﻿namespace Project2_Group_7
 {
     public static class XMLExtension
     {
-        public static void WriteStartDocument (this StreamWriter writer)
+        public static void WriteStartDocument(this StreamWriter writer)
         {
             writer.WriteLine("<?xml version=\"1.0\" encoding=\"UTF-8\"?>");
         }
+
         public static void WriteStartRootElement(this StreamWriter writer)
         {
             writer.WriteLine("<root>");
         }
+
         public static void WriteEndRootElement(this StreamWriter writer)
         {
             writer.WriteLine("</root>");
         }
-        public static void WriteStartElement(this StreamWriter writer)
-        {
-            writer.WriteLine("\t<element>");
-        }
-        public static void WriteEndElement(this StreamWriter writer)
-        {
-            writer.WriteLine("\t</element>");
-        }
-        public static void WriteAttribute(this StreamWriter writer)
-        {
-            //Write attributes to XML file
-            throw new NotImplementedException();
 
+        public static void WriteStartElement(this StreamWriter writer, string elementName)
+        {
+            writer.WriteLine("\t<" + elementName + ">");
+        }
+
+        public static void WriteEndElement(this StreamWriter writer, string elementName)
+        {
+            writer.WriteLine("\t</" + elementName + ">");
+        }
+
+        public static void WriteAttribute(this StreamWriter writer, string attributeName, string attributeValue)
+        {
+            writer.WriteLine(attributeName + "=\"" + attributeValue + "\"");
         }
     }
 }
